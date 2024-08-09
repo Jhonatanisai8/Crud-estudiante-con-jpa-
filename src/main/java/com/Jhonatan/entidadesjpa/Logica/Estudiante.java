840c3c93f69fb7b0679e13500fa138b24121950b
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-09 11:37:17 -0500

    agregaciond de entidades

---
+++ src/main/java/com/Jhonatan/entidadesjpa/Logica/Estudiante.java
@@ -0,0 +1,45 @@
+package com.Jhonatan.entidadesjpa.Logica;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estudiante")
+public class Estudiante {
+
+    @Id
+    @Column(name = "id_estudiante")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int idEstudiante;
+    @Column(name = "nombre")
+    private String nombre;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(int idEstudiante, String nombre) {
+        this.idEstudiante = idEstudiante;
+        this.nombre = nombre;
+    }
+
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
