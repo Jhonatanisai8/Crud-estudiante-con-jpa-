20d9fbc0171deb6add1d332e8ee721d9765ac842
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-08-11 18:03:31 -0500

    haciendo pruebaspra insertar estudiantes

--- src/main/java/com/Jhonatan/entidadesjpa/EntidadesJPA.java
+++ src/main/java/com/Jhonatan/entidadesjpa/EntidadesJPA.java
@@ -1,8 +1,16 @@
 package com.Jhonatan.entidadesjpa;
 
+import com.Jhonatan.entidadesjpa.Logica.Estudiante;
+import com.Jhonatan.entidadesjpa.Logica.EstudianteDao;
+
 public class EntidadesJPA {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EstudianteDao estudianteDao = new EstudianteDao();
+        Estudiante e1 = new Estudiante();
+        e1.setNombre("Jhonatan");
+    
+
+        estudianteDao.insertarEstudiante(e1);
     }
 }
